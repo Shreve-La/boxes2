a5fc9aa0be8ea13404b3d76c1d72bac3aa8a3264
Shreve-La <shrevela@gmail.com>
2017-06-27 10:02:22 -0400

    Initial Commit

---
+++ Boxes2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Boxes2
+//
+//  Created by swcl on 2017-06-27.
+//  Copyright © 2017 Shreve.ca. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
